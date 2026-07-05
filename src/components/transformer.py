f177f42797f73d6fbc2c9d9cc10e388ba1606bca
nishant <irritatednishant@gmail.com>
2026-07-05 13:09:35 +0530

    transformer module

--- src/components/transformer.py
+++ src/components/transformer.py
@@ -12,7 +12,7 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
-
+from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
@@ -54,37 +54,47 @@ class DataTransformation:
         
 
     def initiate_data_transformation(self, train_path, test_path ):
-            test_df = pd.read_csv(test_path)
-            train_df = pd.read_csv(train_path)
+            try:
+                    test_df = pd.read_csv(test_path)
+                    train_df = pd.read_csv(train_path)
 
-            logging.info("Read train and test data completed")
+                    logging.info("Read train and test data completed")
 
-            logging.info("Obtaining preprocessing object")
+                    logging.info("Obtaining preprocessing object")
 
-            preprocessor_obj = self.get_data_transformer_object()
-            numerical_column = ['Tenure Months', 'Monthly Charges', 'CLTV', 'Total Charges']
-            categorical_column = ['Senior Citizen', 'Partner', 'Dependents', 'Internet Service', 'Online Security', 'Online Backup', 'Device Protection', 'Tech Support', 'Contract', 'Paperless Billing', 'Payment Method']
-            target_column = "Churn Value"
-            input_columns = numerical_column + categorical_column
-            X_train = train_df[input_columns]
-            X_test = test_df[input_columns]
+                    preprocessor_obj = self.get_data_transformer_object()
+                    numerical_column = ['Tenure Months', 'Monthly Charges', 'CLTV', 'Total Charges']
+                    categorical_column = ['Senior Citizen', 'Partner', 'Dependents', 'Internet Service', 'Online Security', 'Online Backup', 'Device Protection', 'Tech Support', 'Contract', 'Paperless Billing', 'Payment Method']
+                    target_column = "Churn Value"
+                    input_columns = numerical_column + categorical_column
+                    X_train = train_df[input_columns]
+                    X_test = test_df[input_columns]
 
-            y_train = train_df[target_column]
-            y_test = test_df[target_column]
+                    y_train = train_df[target_column]
+                    y_test = test_df[target_column]
 
-            logging.info(
-                f"Applying preprocessing object on training dataframe and testing dataframe."
-            )
+                    logging.info(
+                        f"Applying preprocessing object on training dataframe and testing dataframe."
+                    )
 
-            X_train_arr = preprocessor_obj.fit_transform(X_train)
-            X_test_arr = preprocessor_obj.transform(X_test)
+                    X_train_arr = preprocessor_obj.fit_transform(X_train)
+                    X_test_arr = preprocessor_obj.transform(X_test)
 
-            train_arr = np.c_[X_train_arr, np.array(y_train)]
-            test_arr = np.c_[X_test_arr, np.array(y_test)]
+                    train_arr = np.c_[X_train_arr, np.array(y_train)]
+                    test_arr = np.c_[X_test_arr, np.array(y_test)]
 
-            logging.info(f"Saved preprocessing object.")
+                    logging.info(f"Saved preprocessing object.")
 
-
+                    save_object(self.data_transformation_config.preprocessor_obj_file_path,
+                                preprocessor_obj
+                            )
+                    return (
+                            train_arr,
+                            test_arr,
+                            self.data_transformation_config.preprocessor_obj_file_path
+                        )
+            except Exception as e:
+                raise CustomException(e, sys)
 
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,19 @@
 import os
-import pickle
 import sys
+
+import joblib
+
 from src.exception import CustomException
 
 
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        joblib.dump(obj, file_path)
+
+    except Exception as e:
+        raise CustomException(e, sys)
